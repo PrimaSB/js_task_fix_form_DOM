1ac0e3a18f000fb9f3298e51625114ca6bd3bad2
Anna Pryimak <sapfira03@gmail.com>
2026-06-28 23:54:49 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+for (const form of forms) {
+  const allInputs = form.querySelectorAll('input');
+
+  for (const element of allInputs) {
+    const label = document.createElement('label');
+    const parentElement = element.parentElement;
+
+    label.className = 'field-label';
+    label.htmlFor = element.id;
+    label.textContent = element.name;
+
+    element.placeholder = element.name.toUpperCase();
+
+    parentElement.appendChild(label);
+  }
+}
