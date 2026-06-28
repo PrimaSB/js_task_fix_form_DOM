11d67f803125d1f0c7ffd9860e931d2c4fb387db
Anna Pryimak <sapfira03@gmail.com>
2026-06-29 00:13:41 +0300

    make build

---
+++ js_task_fix_form_DOM.f75de5e1.js
@@ -0,0 +1,16 @@
+'use strict';
+const forms = document.querySelectorAll('form');
+for (const form of forms){
+    const allInputs = form.querySelectorAll('input');
+    for (const element of allInputs){
+        const label = document.createElement('label');
+        const parentElement = element.parentElement;
+        label.className = 'field-label';
+        label.htmlFor = element.id;
+        label.textContent = element.name;
+        element.placeholder = element.name[0].toUpperCase() + element.name.slice(1).toLowerCase();
+        parentElement.appendChild(label);
+    }
+}
+
+//# sourceMappingURL=js_task_fix_form_DOM.f75de5e1.js.map
